c908cd339f997731e30193a3a4b5b8ef6971f8e0
Millyn <9047230@qq.com>
2014-07-02 12:42:23 +0800

    第一章-处理更多的错误类型

--- Chapter4/1.3.py
+++ Chapter4/1.3.py
@@ -4,8 +4,8 @@ try:
     date = open('missing.txt')  #尝试打开一个不存在的文件
     print (date.readline(),end='')  #并打印 这一步并不会执行 因为他出错了
 
-except IOError:
-    print ('file error')    #错误类型是IOError 所以打印错误提示
+except IOError as err:  #指定err对象显示确切的错误消息
+    print ('file error' + str(err))    #错误类型是IOError 所以打印错误提示 + str(err) 使err 为str字符串 并打印
 
 finally:
     if 'date' in locals():  #无论文件是否存在我们都要做好关闭文件的准备,但如果文件不存在时
