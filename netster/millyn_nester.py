1fd40e39e6e8a079edb61757c76274ffcec56da2
Millyn <9047230@qq.com>
2014-07-02 12:42:10 +0800

    1.4.0版本改动新的函数

--- netster/millyn_nester.py
+++ netster/millyn_nester.py
@@ -1,23 +1,21 @@
 #coding=utf-8
 #Head_First_Python第一章-一个函数
+import sys
 '''
 此函数作用为打印嵌套列表内的所有数据,
 list = the_list
 level = 嵌套列表的制表符参数 填写0即可 每一层list增加1
 indent = 是否使用TAB制表符 默认为 否 传递参数时 0代表否 1或以上代表真
+edit = 插入的位置 默认值为sys.stdout 是指没有指定对象时依然写至屏幕
 '''
-def print_lol(the_list,indent=False,level=0):
+def print_lol(the_list,indent=False,level=0,edit=sys.stdout):
     for each_item in the_list:
         if isinstance(each_item,list):
-            print_lol(each_item,indent,level+1)
+            print_lol(each_item,indent,level+1,edit)
         else:
             if indent:
                 for tab_stop in range(level):
-                    print ('\t', end='')
-            print (each_item)
+                    print ('\t', end='',file=edit)
+            print (each_item,file=edit)
 
-movies = ["The hold Graill", 1975 ,"Terry Jones & Terry Gilliam", 91,\
-         ["Graham Chapman",["Micheal Palin", "John Cleese","Terry Gilliam",\
-         "Eric Idie", "Terry Jones"]]]
-
-print_lol(movies,0)
+print_lol()
